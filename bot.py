9335879448770919c1b2ea97a99fd25aa4a50021
Lewulf <Lewulf@protonmail.com>
2020-05-11 12:55:00 +1000

    woof woof

--- bot.py
+++ bot.py
@@ -1,1 +1,12 @@
-//changing the initial comment
+#is this discord.py?
+import discord
+
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+    print(message.content)
+    if message.content.find("woof.woof") != -1:
+        await message.channel.send("woof woof")
+
+client.run("NzAxNDE2ODM4NzU5NzEwODEw.XpxP1g.P9sieWx_TGclIs4iBIFDOoAwgXw")
